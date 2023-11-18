4ac7f0c8670ac653eff8aef0621fc9d99fc9d1fd
xuantruong1106 <luomtruong11@gmail.com>
2023-11-18 13:36:27 +0700

    programing p2p tcp

--- src/main/java/p2p/Client.java
+++ src/main/java/p2p/Client.java
@@ -4,43 +4,10 @@
  */
 package p2p;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
-import java.util.Scanner;
-
 /**
  *
  * @author My
  */
 public class Client {
-     public static void main(String[] args) {
-        try {
-            Scanner in = new Scanner(System.in);
-            System.out.print("Enter your name: ");
-            String name = in.nextLine().trim();
-            InterServer chatServer = (InterServer) Naming.lookup("rmi://localhost/ABC");
-            InterClientImp chatClient = new InterClientImp();
-            UnicastRemoteObject.exportObject((Remote) chatClient, 0);
-            chatServer.registerClient(name, chatClient);
-            String msg;
-            msg = "[" + name + "] got connected";
-            chatServer.unicastMessage(msg);
-            while (true) {
-                msg = in.nextLine().trim();
-                if (msg.equals("exit")) {
-                    break;
-                }
-                msg = "[From " + name + "] " + msg;
-                chatClient.client.retrieveMessage(msg);
-            }
-            System.out.println("End ");
-            System.exit(0);
-        } catch (MalformedURLException | NotBoundException | RemoteException e) {
-            System.out.println("[System] Server failed: " + e);
-        }
-    }
+    
 }
--- src/main/java/p2p/Server.java
+++ src/main/java/p2p/Server.java
@@ -4,27 +4,49 @@
  */
 package p2p;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.server.UnicastRemoteObject;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 /**
  *
  * @author My
  */
 public class Server {
-    public static void main(String[] args) {
-        try {
-            LocateRegistry.createRegistry(1099); 
-            InterServerImp chatServer = new InterServerImp();
-            UnicastRemoteObject.exportObject(chatServer, 0); 
-            String url = "rmi://localhost/ABC";
-            Naming.rebind(url, chatServer);
-            System.out.println("[Server] is ready..."); 
-        } catch (RemoteException | MalformedURLException e) {
-            System.out.println("[System] Server failed: " + e);
-        } 
+
+    public Server() throws Exception {
+        String from_client;
+        String to_client;
+        Integer port = 1106;
+
+        ServerSocket serversocker = new ServerSocket(port);
+        Socket socket = serversocker.accept();
+        System.out.println("successfull" + socket);
+        while (true) {
+
+            BufferedReader inFromClient
+                    = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            //Tạo outputStream, nối tới socket
+            DataOutputStream outToClient
+                    = new DataOutputStream(socket.getOutputStream());
+
+            //Đọc thông tin từ socket
+            from_client = inFromClient.readLine();
+
+            to_client = from_client + " (Server accepted!)" + '\n';
+            //ghi dữ liệu ra socket
+            outToClient.writeBytes(to_client);
+            
+        }
     }
+
+    public static void main(String[] args) throws Exception {
+        Server server = new Server();
+        return;
+
+    }
+
 }

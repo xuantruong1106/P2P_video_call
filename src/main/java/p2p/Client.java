c63247107e5560832f195b3df2fa0ba3c83bdf1d
xuantruong1106 <luomtruong11@gmail.com>
2023-11-21 14:09:51 +0700

    giao tiếp thành công

--- src/main/java/p2p/Client.java
+++ src/main/java/p2p/Client.java
@@ -9,100 +9,128 @@ import java.util.Scanner;
 public class Client {
 
     public static void main(String argv[]) throws Exception {
+        new Thread(() -> {
+            Scanner sc = new Scanner(System.in);
 
-        Scanner sc = new Scanner(System.in);
-
-        try {
-            // Connect to the server
-            Socket socketServer = new Socket("localhost", 1106);
+            try {
+                // Connect to the server
+                Socket socketServer = new Socket("localhost", 1106);
 
 //            if (socketServer.isConnected()) {
-            DataInputStream din = new DataInputStream(socketServer.getInputStream());
-            DataOutputStream dos = new DataOutputStream(socketServer.getOutputStream());
+                DataInputStream din = new DataInputStream(socketServer.getInputStream());
+                DataOutputStream dos = new DataOutputStream(socketServer.getOutputStream());
 
-            while (true) {
-                boolean checkStateListPort = din.readBoolean();
+                while (true) {
+                    boolean checkStateListPort = din.readBoolean();
 
-                if (checkStateListPort) {
-                    System.out.println(din.readUTF());
+                    if (checkStateListPort) {
+                        System.out.println(din.readUTF());
 
-                    System.out.println("enter port join meet");
-                    int port = sc.nextInt();
-                    try {
-                        Socket socketClient = new Socket("localhost", port);
+                        System.out.print("enter port join meet");
+                        String port = sc.nextLine();
+                        
+                        int portInt = Integer.parseInt(port);
+                         
+                        try {
+                            Socket socketClient = new Socket("localhost", portInt);
 
-                        DataInputStream din1 = new DataInputStream(socketClient.getInputStream());
-                        DataOutputStream dos1 = new DataOutputStream(socketClient.getOutputStream());
+                            DataInputStream din1 = new DataInputStream(socketClient.getInputStream());
+                            DataOutputStream dos1 = new DataOutputStream(socketClient.getOutputStream());
+                            
+                            System.out.print("enter name");
+                            String name1 = sc.nextLine();
+                            dos1.writeUTF(name1);
+                        
+                            while (true) {
+                                System.out.println("client aswer: " + din1.readUTF());
 
-                        System.out.println(din1.readUTF());
-                    } catch (Exception e) {
-                        System.out.println("don't access socket");
-                        System.out.println(" socketClient - 36");
-                    }
-                } else {
-                    System.out.println(din.readUTF());
+                                System.out.print("enter message to client: ");
+                                String message = sc.nextLine();
 
-                    System.out.println("enter name");
-                    String name = sc.nextLine();
-                    dos.writeUTF(name);
+                                dos1.writeUTF(message);
+                            }
 
-                    System.out.println("enter port create server meet");
-                    int port = sc.nextInt();
-                    dos.writeInt(port);
-
-                    // Corrected code for ServerSocket instantiation
-                    ServerSocket sSClient = new ServerSocket(port);
-
-                    try {
-                        Socket sClinet = sSClient.accept();
-
-                        DataInputStream din2 = new DataInputStream(sClinet.getInputStream());
-                        DataOutputStream dos2 = new DataOutputStream(sClinet.getOutputStream());
-
-                        dos2.writeUTF("hello client 2");
-                        while (true) {
-                            System.out.println("client aswer: " + din2.readUTF());
-
-                            System.out.println("enter message to client: ");
-                            String message = sc.nextLine();
-
-                            dos2.writeUTF(message);
+                        } catch (Exception e) {
+                            System.out.println("don't access socket");
+                            System.out.println(" socketClient - 36");
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        System.out.println(" sClinet - 71");
+                    } else {
+                        System.out.println(din.readUTF());
+
+                        System.out.print("enter name: ");
+                        String name = sc.nextLine();
+                        dos.writeUTF(name);
+
+                        System.out.print("enter port create server meet: ");
+                        String port = sc.nextLine();
+                        int portInt = Integer.parseInt(port);
+                        
+                        dos.writeInt(portInt);
+                        
+                        
+                        // Corrected code for ServerSocket instantiation
+                        ServerSocket sSClient = new ServerSocket(portInt);
+
+                        try {
+                            Socket sClinet = sSClient.accept();
+
+                            DataInputStream din2 = new DataInputStream(sClinet.getInputStream());
+                            DataOutputStream dos2 = new DataOutputStream(sClinet.getOutputStream());
+                            
+                            String clientName = din2.readUTF();
+                            System.out.print("Client connected: " + clientName);
+                            dos2.writeUTF("hello client " + clientName);
+                            
+                            while (true) {
+                                System.out.println("client aswer: " + din2.readUTF());
+
+                                System.out.print("enter message to client: ");
+                                String message = sc.nextLine();
+
+                                dos2.writeUTF(message);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            System.out.println(" sClinet - 71");
+                        }
                     }
                 }
-            }
 //            } else {
 //                System.out.println("Cannot connect to the server. Please try again.");
 //                // Add any additional logic you want for this condition.
 //            }
 
-        } catch (Exception e) {
+            } catch (Exception e) {
 
-            System.out.println("enter port join meet");
-            int port = sc.nextInt();
-            try {
-                Socket socketClient = new Socket("localhost", port);
+                System.out.println("enter port join meet: ");
+                int port = sc.nextInt();
+                try {
+                    Socket socketClient = new Socket("localhost", port);
 
-                DataInputStream din2 = new DataInputStream(socketClient.getInputStream());
-                DataOutputStream dos2 = new DataOutputStream(socketClient.getOutputStream());
+                    DataInputStream din2 = new DataInputStream(socketClient.getInputStream());
+                    DataOutputStream dos2 = new DataOutputStream(socketClient.getOutputStream());
+                    
+                    System.out.println("enter name: ");
+                    String name = sc.nextLine();
+                    dos2.writeUTF(name);
+                    
+                    while (true) {
+                        System.out.println(" client aswer: " + din2.readUTF());
 
-                while (true) {
-                    System.out.println(" client aswer: " + din2.readUTF());
+                        System.out.println("enter message to client: ");
+                        String message = sc.nextLine();
 
-                    System.out.println("enter message to client: ");
-                    String message = sc.nextLine();
+                        dos2.writeUTF(message);
+                    }
 
-                    dos2.writeUTF(message);
+                } catch (Exception e1) {
+                    System.out.println("don't access socket");
+                    System.out.println(" socketClient - 94");
                 }
 
-            } catch (Exception e1) {
-                System.out.println("don't access socket");
-                System.out.println(" socketClient - 94");
             }
 
-        }
+        }).start();
+
     }
 }
